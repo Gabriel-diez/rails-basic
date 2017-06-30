0897e6157fea12afd9e650b97c5d1115024d49b2
Diez Gabriel <diez.gabriel64@gmail.com>
2017-06-30 10:47:16 +0200

    create post controller and crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
     resources :users
   end
-  root "admin/users#index"
+  root "posts#index"
 end
