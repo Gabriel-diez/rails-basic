bb841637ba6fee33ca38391a61a52481726764b8
Diez Gabriel <diez.gabriel64@gmail.com>
2017-07-14 15:19:47 +0200

    add fields to sign up and validation for field

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,5 +29,10 @@ class User < ApplicationRecord
 
   mount_uploader :avatar, AvatarUploader
 
+  validates :firstname, :lastname, :email, :password, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: { minimum: 6 }
+  validates_format_of :email, :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/
+
   has_many :posts
 end
