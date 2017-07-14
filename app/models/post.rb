55c8273a63e2857f51d1b8e7665d97b955320902
Diez Gabriel <diez.gabriel64@gmail.com>
2017-07-14 13:51:36 +0200

    add annotation to model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  picture    :string
+#  user_id    :integer
+#
+
 class Post < ApplicationRecord
 
   mount_uploader :picture, AvatarUploader
